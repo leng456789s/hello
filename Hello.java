f3d6ae301e50cf00031e20d615fa82262c2e9c60
leng <2110955743@qq.com>
2023-04-10 17:07:13 +0800

    为了演示git commit 不添加m参数，做的测试：

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("hello world line 1");
+
+		System.out.println("hello world line 2");
+		System.out.println("hello world line 3");
+		System.out.println("hello world line 4");
+		System.out.println("hello world line 5");
+		System.out.println("hello world line 6");
+		System.out.println("hello world line 7");
+	}
+}
